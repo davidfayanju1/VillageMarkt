9e1ecd5a28ac4e1e43b5f6558468da637ae65eea
davidfayanju1 <davidfayanju01@gmail.com>
2024-04-11 08:35:00 +0100

    slight tweaks

--- src/components/home/Carousel.jsx
+++ src/components/home/Carousel.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductCard from "./ProductCard";
+import { motion } from "framer-motion";
 
 const carousel = () => {
+  const [clockWise, setClockwise] = useState(false);
+
   return (
     <div className="w-full md:min-h-[42rem] bg-primary">
-      <div className="asterisk_container flex items-end justify-end md:px-[3.3rem] mb-[-3rem] w-full">
-        <div className="asterisk_content h-[10rem] w-[10rem] rounded-full bg-light-green-2 flex items-center justify-center z-[2]">
+      <section className="asterisk_container flex items-end justify-end md:px-[3.3rem] mb-[-3rem] w-full">
+        <motion.div
+          onClick={() => setClockwise(!clockWise)}
+          className="asterisk_content cursor-pointer h-[10rem] w-[10rem] rounded-full bg-light-green-2 flex items-center justify-center z-[2]"
+          animate={{ rotate: clockWise ? 10 : 0 }}
+          transition={{ ease: "linear", duration: 0.2 }}
+        >
           <span className="font-cooper text-[1.2rem] font-medium w-[50%] text-center text-white">
             Certified Fresh
           </span>
-        </div>
-      </div>
+        </motion.div>
+      </section>
       {/* carousel section */}
-      <div className="carousel-container">
+      <section className="carousel-container">
         <ProductCard />
-      </div>
+      </section>
     </div>
   );
 };
